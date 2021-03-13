9b38fe7541ad8f3b58beb19c0fdb0dd57514a518
bishun945 <cookie_bis@qq.com>
2021-03-13 14:47:51 +0800

    Update hexSticker_FCMm.R

--- hexSticker_FCMm.R
+++ hexSticker_FCMm.R
@@ -14,7 +14,8 @@ data("Bi_clusters")
 colnames(Rrs_clusters.default) %<>% gsub("X", "", .)
 Area <- trapz2(Rrs_clusters.default)
 
-w = c(1, 3, 5, 7, 6)
+w = c(1, 3, 5, 6)
+# w = 3
 dt <- {Rrs_clusters.default[w, ] / Area[w]} %>% 
   tibble::rownames_to_column(., var = "ID") %>%
   reshape2::melt(., id = "ID")
@@ -22,40 +23,55 @@ dt$variable %<>% as.vector() %>% as.numeric()
 dt$ID %<>% as.factor()
 
 # hcl.pals()
+cp.fun <- ggsci::pal_aaas()
 
 ggplot(data = dt, aes(x = variable, y = value, color = ID, group = ID)) + 
-  geom_path(size = 2, lineend = "round") + 
+  geom_path(size = 1.5, lineend = "round") + 
   scale_color_manual(values = length(w) %>%
                        # FCMm::Spectral()
-                       hcl.colors(., "Cork")
-                     ) + 
+                       # hcl.colors(., "Cork")
+                       cp.fun
+                     ) +
   theme_void() + 
   theme(legend.position = "none") +
   theme_transparent() -> p
 
-p
+# p <- ggplot(data = dt, aes(x = variable, y = value, group = ID)) + 
+#   geom_path(size = 1.5, lineend = "round", color = "navy") + 
+#   theme_void()
+
+p + theme(legend.position = "right") + 
+  scale_color_manual(values = c("#3B4992FF", "#EE0000FF", "#008B45FF", "#008280FF", "#631879FF"))
+
+p <- p + scale_color_manual(values = c("#3B4992FF", "#EE0000FF", "#008B45FF", "#008280FF", "#631879FF"))
 
 ## use the ggplot2 example
 sticker(p, 
         package="FCMm", 
-        p_size=26, p_x = 1, p_y = 1.5, 
+        p_size=28, p_x = 1, p_y = 1.4, 
         p_family = "mont", p_fontface = "bold",
-        s_x=1, s_y=.85, s_width=1.3, s_height=1,
-        h_fill = "#1D1B1B", h_color = "gray75", p_color = "#FFFFFF",
+        s_x=1, s_y=.85, s_width=1.3, s_height=0.8,
+        h_fill = "#FDFFE1", h_color = "black", p_color = "black", h_size = 1, 
+        # h_fill = "#007DFF", h_color = "black", p_color = "#FF8200",
         # h_fill = "#8AAAE5", h_color = "gray75", p_color = "#FFFFFF",
         spotlight = TRUE, l_x = 1, l_y = 0.5, l_alpha = 0.4,
-        filename = "logo.png"
+        filename = "./man/figures/logo.png"
         )
 
 sticker(p, 
         package="FCMm", 
-        p_size=9, p_x = 1, p_y = 1.5, 
+        p_size=28, p_x = 1, p_y = 1.4, 
         p_family = "mont", p_fontface = "bold",
-        s_x=1, s_y=.85, s_width=1.3, s_height=1,
-        h_fill = "#1D1B1B", h_color = "gray75", p_color = "#FFFFFF",
+        s_x=1, s_y=.85, s_width=1.3, s_height=0.8,
+        h_fill = "#FDFFE1", h_color = "black", p_color = "black", h_size = 1, 
+        # h_fill = "#007DFF", h_color = "black", p_color = "#FF8200",
         # h_fill = "#8AAAE5", h_color = "gray75", p_color = "#FFFFFF",
         spotlight = TRUE, l_x = 1, l_y = 0.5, l_alpha = 0.4,
-        filename = "logo.svg"
+        filename = "./man/figures/logo.svg"
 )
+
+
+
+
 
 
